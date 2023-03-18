d4c634cb43cc254c47456c1ea9abcf25ffe67c25
anuj.agrawal_cs20@gla.ac.in <anujagrawal601@gmail.com>
2023-03-19 02:04:18 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
+// // // var React = require('react');
+// import React from 'react';
+// // // var ReactDOM = require('react-dom');
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render([
+// <h1>thapa technical </h1>,
+// <p>Plz like and share</p>
+// ],
+// document.getElementById('root'));
+
+// // //we can also do it by using : 
+
+// // // var h1 = document.createElement('h1');
+// // // h1.innerHTML = "Hello World";
+// // // document.getElementById('root').appendChild(h1);
+
+
+
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const name="AKA";
+// ReactDOM.render(
+//     <>
+//     <h1>my name is {name}</h1>
+//     <p>sum of 2 and 3 is {2+3}</p>
+//     </>,
+//     document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// const fname="Anuj";
+// const lname="Agr";
+// ReactDOM.render(
+//     <h1>This is {`${fname} ${lname}`}</h1>,
+//     document.getElementById('root')
+// );
+
+// const curdate=new Date().toLocaleDateString();
+// const curtime=new Date().toLocaleTimeString();
+// const name="Anuj Agrawal"
+// ReactDOM.render([
+//     <h1>My name is {name}</h1>,
+//     <p>Today's date is {curdate}</p>,
+//     <p>Today's date is {curtime}</p>
+// ],
+//     // <p>`${curdate}`</p>
+// document.getElementById('root'));
+
+const name="AKA";
+const image="https://picsum.photos/200";
+const image1="https://picsum.photos/250";
+const link="https://www.thapatechnical.com/"
+ReactDOM.render(<>
+    <h1 style="color:#fa9191;">My name is {name}</h1>
+    <div className='img_div'>
+    <img src={image} alt="pic"/>
+    
+    <a href={link} target="_thapa">
+        <img src={image1}/>
+    </a>
+    </div>
+    </>
+    ,
+    document.getElementById('root')
+) 
